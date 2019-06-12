57cb07fbd0ae4c545e4a9a7a3aa4a1ce23b2dc2a
agaricide <agaricide@gmail.com>
2019-06-11 23:04:15 -0500

    Added two markers to the map

--- gmap-polygons/src/index.ts
+++ gmap-polygons/src/index.ts
@@ -4,4 +4,20 @@ console.log("Injecting map!");
 
 (async () => {
   const map = await injectMap();
+  const coords = { lat: 32.78391, lng: -96.765375 };
+
+  new google.maps.Marker({
+    position: coords,
+    map: map,
+    title: "Hello World!"
+  });
+
+  const coords2 = { lat: 32.787682, lng: -96.784360 };
+
+  new google.maps.Marker({
+    position: coords2,
+    map: map,
+    title: "Hello World!"
+  });
+
 })();
