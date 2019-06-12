2db021bef4e9ec0d4928a6294ad44aadb54c38ae
agaricide <agaricide@gmail.com>
2019-06-11 23:05:59 -0500

    Adding implementation notes for findCrossStreet.ts

---
+++ gmap-polygons/src/services/findCrossStreets.ts
@@ -0,0 +1,5 @@
+// Example string search GET:
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=YOUR_API_KEY
+
+// See:
+// https://developers.google.com/places/web-service/search
