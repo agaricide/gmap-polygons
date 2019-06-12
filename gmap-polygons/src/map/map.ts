d4a77b79768816fbe89aa841ab5a548a83f97b2e
agaricide <agaricide@gmail.com>
2019-06-12 00:57:47 -0500

    Cleaner injectMap implementation

--- gmap-polygons/src/map/map.ts
+++ gmap-polygons/src/map/map.ts
@@ -1,17 +1,15 @@
-import "map.css";
+import "./map.css";
 
-type GMap = google.maps.Map;
-
-const createDiv = (id = "") => {
-  const div: HTMLDivElement = document.createElement("div");
+const createDiv = (id = ""): HTMLDivElement => {
+  const div = document.createElement("div");
   div.id = id;
   document.body.appendChild(div);
   return div;
 };
 
-const initMap = () => {
+const initMap = (): Promise<google.maps.Map> => {
   const div = createDiv("map");
-  const map: GMap = new google.maps.Map(div, {
+  const map = new google.maps.Map(div, {
     center: { lat: 32.7767, lng: -96.797 },
     zoom: 15
   });
@@ -22,18 +20,17 @@ const googleMapsUrl =
   "https://maps.googleapis.com/maps/api/js?key=AIzaSyDLfK6Ay-NkW5FRc-mmb82-nsSYWy9m0po&libraries=drawing";
 
 const injectMap = () =>
-  new Promise<GMap>((resolve, reject) => {
+  new Promise<google.maps.Map>((resolve, reject) => {
     if (!document.querySelectorAll(`[src="${googleMapsUrl}"]`).length) {
-      document.body.appendChild(
-        Object.assign(document.createElement("script"), {
-          type: "text/javascript",
-          src: googleMapsUrl,
-          onload: () =>
-            initMap()
-              .then(map => resolve(map))
-              .catch(err => reject(err))
-        })
-      );
+      const el = document.createElement("script");
+      el.type = "text/javascript";
+      el.src = googleMapsUrl;
+      el.onload = () =>
+        initMap()
+          .then(map => resolve(map))
+          .catch(err => reject(err));
+
+      document.body.appendChild(el);
     }
   });
 
