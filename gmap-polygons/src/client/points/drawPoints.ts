fcc334afbcf66a24cf70c99e8e01b37f5bfa1b37
agaricide <agaricide@gmail.com>
2019-06-18 00:17:21 -0500

    Successfully drew active calls on map

--- gmap-polygons/src/client/points/drawPoints.ts
+++ gmap-polygons/src/client/points/drawPoints.ts
@@ -1,10 +1,28 @@
-import points from "./points.json";
+// import points from "./points.json";
+import dump from "./dump.json";
+import moment from "moment";
+
+const points = dump
+  .filter(({ location }) => location && location.lat && location.lng)
+  .filter(incident => incident.location)
+  .map(({ location, incident }) => ({ ...incident, ...location }));
 
 const drawPoints = (map: google.maps.Map): google.maps.Marker[] =>
-  points.map(({ lat, lng }, index) => {
+  points.map(({ lat, lng, nature_of_call, date_time }, index) => {
     const position = { lat, lng };
-    const title = `Point #${index}`;
-    return new google.maps.Marker({ map, position, title });
+    const title = nature_of_call;
+    const marker = new google.maps.Marker({ map, position, title });
+    const infoWindow = new google.maps.InfoWindow({
+      content: `<div>
+        ${nature_of_call}
+        ${moment(date_time).fromNow()}
+      </div>`
+    });
+    google.maps.event.addListener(marker, "click", function() {
+      infoWindow.open(map, marker);
+    });
+
+    return marker;
   });
 
 export default drawPoints;
--- gmap-polygons/src/server/index.ts
+++ gmap-polygons/src/server/index.ts
@@ -1,15 +1,18 @@
 import searchDallas from "./services/searchDallas";
 import getActiceCalls from "./services/getActiveCalls";
 import { Incident } from "./types/incident";
+import { get } from "lodash";
 
-// searchDallas("W Illinois Ave")
-//   .then(response => {
-//     console.log(response.data.candidates[0]);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+type latLng = google.maps.LatLngLiteral;
 
-getActiceCalls().then((data: Incident[]) => {
-  data.map(incident => console.log(incident));
-});
+getActiceCalls()
+  .then((data: Incident[]) => {
+    const tuples = data.map(async incident => {
+      const response = await searchDallas(incident.location);
+      const place = get(response, "data.candidates", [])[0];
+      const location: latLng = get(place, "geometry.location");
+      return { incident, location };
+    });
+    return Promise.all(tuples);
+  })
+  .then(tuples => console.log(JSON.stringify(tuples)));
